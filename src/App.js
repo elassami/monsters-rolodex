ed8a3625f3c8bd7abe6ba7def0cd53c335791043
elassami <assami.younes@gmail.com>
2023-05-31 18:17:41 -0400

    first commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box.component';
+
+class  App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters:[],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json()).then(users => {this.setState({monsters:users})});
+  }
+  render(){
+
+    const {monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()));
+
+    return (
+    <div className="App">
+      <h1>Monsters Rolodex</h1>
+      <SearchBox searchField={searchField} 
+        placeholder='search monsters'
+        handleChange={e => this.setState({searchField: e.target.value})}
+      />
+      <CardList monsters={filteredMonsters}>
+
+      </CardList>
+    </div>
+    );
+  }
+}
+
+export default App;
